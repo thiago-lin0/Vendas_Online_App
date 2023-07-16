2351217fe977d4cadb3f224101c93858db1e4d16
Thiago Lino <thiagolinu@gmail.com>
2023-07-16 09:55:02 -0300

    alterando os erros

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>testando</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
